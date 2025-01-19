0c3da0cb57c7c5841518349d52e89442d8c63300
Kodehawa <david.alejandro.rubio@gmail.com>
2025-01-19 00:08:39 -0300

    add

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "NixOS System Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    wizardlink-neovim.url = "github:wizardlink/linuxware";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      home-manager,
+      nixpkgs,
+      chaotic,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations."Napoli" =
+        let
+          specialArgs = inputs;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = inputs;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.kodehawa = import ./home-manager.nix;
+            }
+            chaotic.nixosModules.nyx-cache
+            chaotic.nixosModules.nyx-overlay
+            chaotic.nixosModules.nyx-registry
+          ];
+        in
+        nixpkgs.lib.nixosSystem { inherit system specialArgs modules; };
+
+      formatter."${system}" = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+    };
+}
